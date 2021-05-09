364143229002f8cc3cd7f8e0a6d2e998e4e1f89e
maazwaqar33 <80854341+maazwaqar33@users.noreply.github.com>
2021-05-09 16:56:38 +0500

    Delete HiddenField.js

--- src/components/Forms/HiddenField.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-const HiddenField = ({ input, type, className }) => (
-  <Fragment>
-    <input {...input} type={type} className={className} />
-  </Fragment>
-);
-
-export default HiddenField;
